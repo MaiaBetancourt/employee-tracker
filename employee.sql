5753d3551e92219b97d064264d85e2a45831be9d
Maria <maria.betancourt15@yahoo.com>
2020-09-08 17:31:38 -0700

    added schemas

--- employee.sql
+++ employee.sql
@@ -1,49 +1,69 @@
-DROP DATABASE IF EXISTS top_songsDB;
-CREATE database top_songsDB;
+DROP DATABASE IF EXISTS employees;
+CREATE database employees;
 
-USE top_songsDB;
+USE employees;
 
-CREATE TABLE top5000 (
-  position INT NOT NULL,
-  artist VARCHAR(100) NULL,
-  song VARCHAR(100) NULL,
-  year INT NULL,
-  raw_total DECIMAL(10,4) NULL,
-  raw_usa DECIMAL(10,4) NULL,
-  raw_uk DECIMAL(10,4) NULL,
-  raw_eur DECIMAL(10,4) NULL,
-  raw_row DECIMAL(10,4) NULL,
-  PRIMARY KEY (position)
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) DEFAULT '' NOT NULL,
+  PRIMARY KEY (id)
+
 );
 
-CREATE TABLE top_albums (
-  position INT NOT NULL,
-  artist VARCHAR(100) NULL,
-  album VARCHAR(100) NULL,
-  year INT NULL,
-  raw_total DECIMAL(10,4) NULL,
-  raw_usa DECIMAL(10,4) NULL,
-  raw_uk DECIMAL(10,4) NULL,
-  raw_eur DECIMAL(10,4) NULL,
-  raw_row DECIMAL(10,4) NULL,
-  PRIMARY KEY (position)
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  PRIMARY KEY (id)
 );
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY (id)
+);
+
 -- * **department**:
-
---   * **id** - INT PRIMARY KEY
---   * **name** - VARCHAR(30) to hold department name
+INSERT INTO department (name) VALUE ("Sales");
+INSERT INTO department (name) VALUE ("Support");
+INSERT INTO department (name) VALUE ("Engineer");
 
 -- * **role**:
+INSERT INTO role (title, salary, department_id) 
+VALUE ("Sales Lead", 70000, 1);
+INSERT INTO role (title, salary, department_id) 
+VALUE ("Sales Rep", 55000, 1);
+INSERT INTO role (title, salary, department_id) 
+VALUE ("Support Lead", 40000, 2);
+INSERT INTO role (title, salary, department_id) 
+VALUE ("Email Support", 60000, 2);
+INSERT INTO role (title, salary, department_id) 
+VALUE ("Tech Support", 90000, 2);
+INSERT INTO role (title, salary, department_id) 
+VALUE ("Senior Engineer", 120000, 3);
+INSERT INTO role (title, salary, department_id) 
+VALUE ("Engineer", 80000, 3);
 
 --   * **id** - INT PRIMARY KEY
---   * **title** -  VARCHAR(30) to hold role title
---   * **salary** -  DECIMAL to hold role salary
---   * **department_id** -  INT to hold reference to department role belongs to
-
--- * **employee**:
-
---   * **id** - INT PRIMARY KEY
---   * **first_name** - VARCHAR(30) to hold employee first name
---   * **last_name** - VARCHAR(30) to hold employee last name
---   * **role_id** - INT to hold reference to role employee has
---   * **manager_id** - INT to hold reference to another employee that manager of the current employee. This field may be null if the employee has no manager
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Aster", "Florez", 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Mike", "Meredith", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Cristina", "Padron", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Luis", "Nunes", 3, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Kaylee", "Medina", 4, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Luis", "Betancourt", 5, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Beatrice", "Santana", 6, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Margarita", "Ramirez", 7, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Raul", "Betancourt", 7, 3);
